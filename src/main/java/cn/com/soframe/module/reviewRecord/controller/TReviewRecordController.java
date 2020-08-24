baf548badff1f885bc71bc1343659712ab72b091
Mr.Fu <896930409@qq.com>
2020-08-24 20:12:25 +0800

    熟悉swagger提出的第一个接口

--- src/main/java/cn/com/soframe/module/reviewRecord/controller/TReviewRecordController.java
+++ src/main/java/cn/com/soframe/module/reviewRecord/controller/TReviewRecordController.java
@@ -2,6 +2,8 @@ package cn.com.soframe.module.reviewRecord.controller;
 
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "测试接口1")
 public class TReviewRecordController {
 
+    @ApiOperation(value = "测试的接口啦",notes = "测试的接口1223442")
+    @GetMapping("/test")
+    public String test(){
+        return "helloworld";
+    }
 }
