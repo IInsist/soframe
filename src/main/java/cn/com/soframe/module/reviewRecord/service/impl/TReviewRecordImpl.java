4bf86880b9fba6878a875d5e16267d3fb698a9fe
Mr.Fu <896930409@qq.com>
2020-08-28 10:57:05 +0800

    Redis调用

--- src/main/java/cn/com/soframe/module/reviewRecord/service/impl/TReviewRecordImpl.java
+++ src/main/java/cn/com/soframe/module/reviewRecord/service/impl/TReviewRecordImpl.java
@@ -3,8 +3,13 @@ package cn.com.soframe.module.reviewRecord.service.impl;
 import cn.com.soframe.module.reviewRecord.entity.TReviewRecord;
 import cn.com.soframe.module.reviewRecord.repository.TReviewRecordRepository;
 import cn.com.soframe.module.reviewRecord.service.TReviewRecordServiceI;
+import cn.com.soframe.utils.RedisUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -15,6 +20,16 @@ import org.springframework.stereotype.Service;
  * @since 2020-08-21
  */
 @Service
+@Slf4j
 public class TReviewRecordImpl extends ServiceImpl<TReviewRecordRepository, TReviewRecord> implements TReviewRecordServiceI {
+
+    @Resource
+    private TReviewRecordRepository reviewRecordRepository;
+
+    /**
+     * 自动装配Redis对象
+     */
+    @Autowired
+    private RedisUtil redisUtil;
 
 }
