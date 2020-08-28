386f301b3bc5df8bef51e7ed8de9808d287e58a2
Mr.Fu <896930409@qq.com>
2020-08-28 11:15:33 +0800

    Redis测试

---
+++ src/test/java/cn/com/soframe/redis/RedisTest.java
@@ -0,0 +1,23 @@
+package cn.com.soframe.redis;
+
+import cn.com.soframe.utils.RedisUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author fujh
+ */
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    RedisUtil redisUtil;
+
+    @Test
+    public void t1(){
+        redisUtil.set("fujh","1540689207");
+        String name = String.valueOf(redisUtil.get("fujh"));
+        System.out.println(name);
+    }
+}
