97259bbd2b6fa6df81a261d4112a6d81d9610d24
Mr.Fu <896930409@qq.com>
2020-09-01 11:51:32 +0800

    自定义日志注解

--- soframe-web/src/main/java/cn/com/soframe/annotation/OperationLog.java
+++ soframe-web/src/main/java/cn/com/soframe/annotation/OperationLog.java
@@ -1,5 +1,18 @@
 package cn.com.soframe.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @author fujh
+ * @since 2020年9月1日11:49:15
+ * 自定义日志管理注解
+ *
+ * 注解解释：
+ *  1、@Target 定义说明 “自定义注解类” 作用于什么地方
+ *  2、
+ */
+@Target(ElementType.ANNOTATION_TYPE)
 public @interface OperationLog {
 
 }
