3f18cf6bdaf31b9f19393692c3c4c661fd5e450c
Mr.Fu <896930409@qq.com>
2020-09-07 15:38:00 +0800

    新增soframe-component

---
+++ soframe-component/src/main/java/cn/com/soframe/SoFrameComponentApplication.java
@@ -0,0 +1,17 @@
+package cn.com.soframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fujh
+ * so-frame框架组件包启动类
+ */
+@SpringBootApplication
+public class SoFrameComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoFrameComponentApplication.class, args);
+    }
+
+}
